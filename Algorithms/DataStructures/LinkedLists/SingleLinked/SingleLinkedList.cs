d97482c41329f69009b4553ef88b33f3e640c04a
Nenad Milosevic <nele.nasa@gmail.com>
2020-01-02 14:24:13 +0100

    SumOfTwoLinkedList, Binarny list to integer, reverse linked list (iterative and recursive)

--- Algorithms/DataStructures/LinkedLists/SingleLinked/SingleLinkedList.cs
+++ Algorithms/DataStructures/LinkedLists/SingleLinked/SingleLinkedList.cs
@@ -22,7 +22,7 @@ namespace DataStructures.LinkedLists.SingleLinked
         {
 
         }
-
+     
         #endregion
 
         #region Properties
@@ -51,6 +51,123 @@ namespace DataStructures.LinkedLists.SingleLinked
             }
 
             return slowerPointer;
+        }
+
+        /// <summary>
+        /// Adding two list.
+        /// Example: (2 -> 4 -> 3) + (5 -> 6 -> 4) = (7 -> 0 -> 8)
+        /// </summary>
+        /// <param name="firstRoot">Root of first single linked list</param>
+        /// <param name="secondRoot">Root of second single linked list</param>
+        /// <returns></returns>
+        public static SingleLinkedList SumOfTwoLinkedList(SingleLinkedList firstList, SingleLinkedList secondList)
+        {
+            SingleLinkedListNode firstRoot = firstList.Root;
+            SingleLinkedListNode secondRoot = secondList.Root;
+            SingleLinkedListNode previousNode = null;
+
+            int step = 0;
+
+            while (firstRoot != null && secondRoot != null)
+            {
+                int currentValue = step + firstRoot.Value + secondRoot.Value;
+                firstRoot.Value = currentValue % 10;
+                step = currentValue / 10;
+
+                if (firstRoot.Next == null || secondRoot.Next == null)
+                    previousNode = firstRoot;
+
+                firstRoot = firstRoot.Next;
+                secondRoot = secondRoot.Next;
+            }
+
+            while (firstRoot != null)
+            {
+                int currentValue = firstRoot.Value + step;
+                firstRoot.Value = currentValue % 10;
+                step = currentValue / 10;
+                previousNode.Next = firstRoot;
+                previousNode = firstRoot;
+                firstRoot = firstRoot.Next;
+            }
+
+            while (secondRoot != null)
+            {
+                int currentValue = secondRoot.Value + step;
+                secondRoot.Value = currentValue % 10;
+                step = currentValue / 10;
+                previousNode.Next = secondRoot;
+                previousNode = secondRoot;
+                secondRoot = secondRoot.Next;
+            }
+
+            if (step == 1)
+                previousNode.Next = new SingleLinkedListNode(step);
+
+            return firstList;
+        }
+
+
+        /// <summary>
+        /// Convert 110 to 6 
+        /// Convert 111 to 7 etc...
+        /// </summary>
+        /// <param name="singleLinkedList"></param>
+        /// <returns></returns>
+        public int BinaryListToInteger()
+        {
+            SingleLinkedListNode current = _root;
+            int result = 0;
+
+            while (current != null)
+            {
+                result = result << 1 | current.Value;
+                current = current.Next;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Reverse linked list
+        /// 1 -> 2 -> 3 -> 4 -> 5  ====> 5 -> 4 -> 3 -> 2 -> 1
+        /// </summary>
+        /// <param name="value"></param>
+        public static SingleLinkedList ReverseLinkedList(SingleLinkedList list)
+        {
+            var result = ReverseLinkedListIterative(null, list.Root);
+            return new SingleLinkedList(result);
+        }
+
+        private static SingleLinkedListNode ReverseLinkedListIterative(SingleLinkedListNode previousNode, SingleLinkedListNode currentNode)
+        {
+            while (currentNode != null)
+            {
+                SingleLinkedListNode nextNode = currentNode.Next;
+                currentNode.Next = previousNode;
+
+                previousNode = currentNode;
+                currentNode = nextNode;
+            }
+
+            return previousNode;
+        }
+
+        private static SingleLinkedListNode ReverseLinkedListRecursive(SingleLinkedListNode previousNode, SingleLinkedListNode currentNode)
+        {
+            if (currentNode == null)
+                return null;
+
+            if (currentNode.Next == null)
+            {
+                currentNode.Next = previousNode;
+                return currentNode;
+            }
+
+            SingleLinkedListNode next = currentNode.Next;
+            currentNode.Next = previousNode;
+
+            return ReverseLinkedListRecursive(currentNode, next);
         }
 
         public void AddToEnd(int value)
